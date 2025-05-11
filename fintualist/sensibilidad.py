3d6ccb500fbde3a5ce3d7e265d8377b9998d0133
Santiago A <sarmstrong@uc.cl>
2025-05-11 17:14:11 -0400

    ADD: modulo de analisisd e sensibilidad

---
+++ fintualist/sensibilidad.py
@@ -0,0 +1,68 @@
+# simulacion_sensibilidad.py (actualizado con Pandas)
+import pandas as pd
+from fintualist.escenarios import (
+    EscenarioArriendo,
+    EscenarioCompra,
+    ParametrosSimulacion,
+)
+from typing import Dict, Any
+
+
+class SimuladorSensibilidad:
+    def __init__(
+        self, num_simulaciones: int, distribuciones_parametros: Dict[str, Any]
+    ):
+        """
+        Inicializa el simulador de sensibilidad.
+
+        Args:
+            num_simulaciones: El número de simulaciones a realizar.
+            distribuciones_parametros: Un diccionario donde las claves son los nombres
+                                       de los parámetros (de ParametrosSimulacion)
+                                       y los valores son objetos que tienen un
+                                       método 'rvs' para generar muestras aleatorias.
+        """
+        self.num_simulaciones = num_simulaciones
+        self.distribuciones_parametros = distribuciones_parametros
+        self.resultados_df = pd.DataFrame()
+
+    def _generar_parametros(self) -> Dict[str, float]:
+        """
+        Genera un diccionario de parámetros muestreados aleatoriamente
+        para la simulación.
+
+        Returns:
+            Un diccionario con los parámetros muestreados.
+        """
+        parametros = {}
+        for nombre, distribucion in self.distribuciones_parametros.items():
+            parametros[nombre] = distribucion.rvs()
+        return parametros
+
+    def ejecutar_simulacion(self):
+        """
+        Ejecuta la simulación de sensibilidad para el número de simulaciones definido.
+        """
+        resultados_arriendo = []
+        resultados_compra = []
+        parametros_simulaciones = []
+        for _ in range(self.num_simulaciones):
+            parametros_dict = self._generar_parametros()
+            parametros = ParametrosSimulacion(**parametros_dict)
+            parametros_simulaciones.append(parametros_dict)
+
+            escenario_arriendo = EscenarioArriendo()
+            resultado_arriendo = escenario_arriendo.calcular_capital_relativo_final(
+                parametros
+            )
+            resultados_arriendo.append(resultado_arriendo)
+
+            escenario_compra = EscenarioCompra()
+            resultado_compra = escenario_compra.calcular_capital_relativo_final(
+                parametros
+            )
+            resultados_compra.append(resultado_compra)
+
+        self.resultados_df = pd.DataFrame(parametros_simulaciones)
+        self.resultados_df["capital_relativo_arriendo"] = resultados_arriendo
+        self.resultados_df["capital_relativo_compra"] = resultados_compra
